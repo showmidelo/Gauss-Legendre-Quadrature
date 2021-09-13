5d17f357daa0a0ad225a372cb3243033f339aa3a
showmidelo <showmidelo@yahoo.com>
2021-09-13 12:27:32 +0430

    Integral

--- Integral.py
+++ Integral.py
@@ -11,3 +11,14 @@ def Legendre(n, x):
         return x
     else:
         return ((2.0 * n - 1.0) * x * Legendre(n - 1, x) - (n - 1) * Legendre(n - 2, x)) / n
+
+
+# Derivative of the Legendre polynomials
+def DLegendre(n, x):
+    x = np.array(x)
+    if n == 0:
+        return x * 0
+    elif n == 1:
+        return x * 0 + 1.0
+    else:
+        return (n / (x ** 2 - 1.0)) * (x * Legendre(n, x) - Legendre(n - 1, x))
