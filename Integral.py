0788246e89b17be66d8234f6473b9f1630482ba1
showmidelo <showmidelo@yahoo.com>
2021-09-13 12:26:36 +0430

    Integral

---
+++ Integral.py
@@ -0,0 +1,13 @@
+import numpy as np
+# calculate Gauss Legendre Quadrature
+
+
+# Recursive generation of the Legendre polynomial of order n
+def Legendre(n, x):
+    x = np.array(x)
+    if n == 0:
+        return x * 0 + 1.0
+    elif n == 1:
+        return x
+    else:
+        return ((2.0 * n - 1.0) * x * Legendre(n - 1, x) - (n - 1) * Legendre(n - 2, x)) / n
